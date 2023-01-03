51e154bab74042e3b1b0f074cfd137d4f172e77b
leigh90 <ashley@angaza.com>
2023-01-03 16:01:02 +0300

    minor changes to test messages

--- smsparser.py
+++ smsparser.py
@@ -7,8 +7,9 @@ from datetime import datetime
 pattern = r"\w+\s+Confirmed\.\s+Ksh(?P<amount_paid>[\d,|\d]+).00\s+\w+\s+\w+\s+(?P<vendor_name>[\w\s]+)[\s|\.]+on\s+(?P<date_paid>[0-9][0-9]/[0-9][0-9]/[0-9][0-9])\s+at\s+(?P<time>[0-9]+:[0-9][0-9]\s+\w+)[\.|\s]+New\s+M-PESA\s+balance\s+is\s+Ksh(?P<mpesa_balance>[\d|,]+\.[\d][\d]+)\.\s+Transaction\scost,\sKsh(?P<transaction_cost>[\d]+\.[0-9][0-9])\..+"
 test_text = """THISCODE Confirmed. Ksh9,000.00 paid to THAT COMPANY LIMITED. on 12/12/22 at
 12:25 AM.New M-PESA balance is Ksh10,508.68. Transaction cost, Ksh0.00. Amount
-you can transact within the day is 290,960.00. Pay with M-PESA GlobalPay virtual
+you can transact within the day is 215,786.00. Pay with M-PESA GlobalPay virtual
 Visa card linked to MPESA wallet. More Promotional Stuff"""
+
 # 1. Read in text file
 def read_sms_file():
     all_texts = []
